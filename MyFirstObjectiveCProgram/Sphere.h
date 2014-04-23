e71768d2b29d8980d929194ffd223b7771cfc320
Adam <adamgoodapp@gmail.com>
2014-04-23 17:29:46 +0100

    Add class header and main

---
+++ MyFirstObjectiveCProgram/Sphere.h
@@ -0,0 +1,13 @@
+//
+//  Sphere.h
+//  MyFirstObjectiveCProgram
+//
+//  Created by Adam on 23/04/2014.
+//  Copyright (c) 2014 treehouse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sphere : NSObject
+
+@end
---
+++ MyFirstObjectiveCProgram/Sphere.m
@@ -0,0 +1,13 @@
+//
+//  Sphere.m
+//  MyFirstObjectiveCProgram
+//
+//  Created by Adam on 23/04/2014.
+//  Copyright (c) 2014 treehouse. All rights reserved.
+//
+
+#import "Sphere.h"
+
+@implementation Sphere
+
+@end
